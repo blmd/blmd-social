466ed739e785ada4e3452fc9f6ab25176b9c4b0a
Casey <caseyjvcain@gmail.com>
2016-01-14 10:06:40 -0500

    Debug

--- blmd-social.php
+++ blmd-social.php
@@ -987,7 +987,7 @@ class BLMD_Social {
 	// }
 	
 	public static function get_compact_number( $full_number, $network = '' ) {
-		// return $full_number;
+		return $full_number;
 		$onum = $full_number;
 		$full_number = (int)$full_number;
 		error_log('formatting: '.$full_number);
